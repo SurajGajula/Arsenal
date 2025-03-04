b52d1d9db50a7136990dceae707b43c607f9b5b1
SurajGajula <ssamgajula@gmail.com>
2025-03-03 23:37:53 -0800

    game

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 import Arsenal from './arsenal.js';
 
+let round = 1;
+let value = 10;
 const arsenal = new Arsenal();
+
+function updateInfoArea() {
+    document.querySelector('.info-area').textContent = `Round: ${round}, Value: ${value}`;
+}
+
+updateInfoArea();
 
 document.querySelector('.arsenal-button').onclick = function() {
     console.log('arsenal');
-    document.querySelector('.display-area').textContent = arsenal.value();
+    const arsenalValue = arsenal.value();
+    document.querySelector('.display-area').textContent = arsenalValue;
+    if (arsenalValue > value) {
+        round += 1;
+        value += 10;
+    } else {
+        round = 1;
+        value = 10;
+    }
+    updateInfoArea();
 };
