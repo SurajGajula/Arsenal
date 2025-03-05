f7cde47231651913ee4736f792c6177a17a3c5f8
SurajGajula <ssamgajula@gmail.com>
2025-03-04 18:04:35 -0800

    UI

--- main.js
+++ main.js
@@ -85,6 +85,6 @@ function showCards() {
 
 function nextRound(arsenalValue, value) {
     const diff = arsenalValue -value;
-    let increment = Math.ceil((10 + round * (diff / round)) / 10) * 10;
+    let increment = Math.ceil((10 + Math.pow(round, 1.5) * Math.pow(diff, 1.5) / 100) / 10) * 10;
     return value + increment;
 }
